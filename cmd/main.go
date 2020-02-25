2dbf2d863ae397c64f345fcc099da74e7d08985a
ProEugene97 <proj9007@gmail.com>
2020-02-25 20:47:11 +0300

    update cmd.main.go

--- cmd/main.go
+++ cmd/main.go
@@ -23,125 +23,6 @@ import (
 	"time"
 )
 
-/*
-var (
-	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-)
-
-func RandStringRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
-
-type MyHandler struct {
-	sessions map[string]uint
-	users    map[string]*User
-}
-
-func NewMyHandler() *MyHandler {
-	return &MyHandler{
-		sessions: make(map[string]uint, 10),
-		users: map[string]*User{
-			"rvasily": {1, "rvasily", "love"},
-		},
-	}
-}
-
-// http://127.0.0.1:8080/login?login=rvsily&password=love
-
-func (api *MyHandler) Login(w http.ResponseWriter, r *http.Request) {
-
-	user, ok := api.users[r.FormValue("login")]
-	//fmt.Println(user, api.users[r.FormValue("login")], api.users["rvasily"],r.FormValue("login"))
-	if !ok {
-		http.Error(w, `no user`, 404)
-		return
-	}
-
-	if user.Password != r.FormValue("password") {
-		http.Error(w, `bad pass`, 400)
-		return
-	}
-
-	SID := RandStringRunes(32)
-
-	api.sessions[SID] = user.ID
-
-	cookie := &http.Cookie{
-		Name:    "session_id",
-		Value:   SID,
-		Expires: time.Now().Add(10 * time.Hour),
-	}
-	http.SetCookie(w, cookie)
-	//w.Write([]byte(SID))
-
-}
-
-func (api *MyHandler) SignUp(w http.ResponseWriter, r *http.Request) {
-
-	user, ok := api.users[r.FormValue("login")]
-	//fmt.Println(user, api.users[r.FormValue("login")], api.users["rvasily"],r.FormValue("login"))
-	if !ok {
-		http.Error(w, `no user`, 404)
-		return
-	}
-
-	if user.Password != r.FormValue("password") {
-		http.Error(w, `bad pass`, 400)
-		return
-	}
-
-	SID := RandStringRunes(32)
-
-	api.sessions[SID] = user.ID
-
-	cookie := &http.Cookie{
-		Name:    "session_id",
-		Value:   SID,
-		Expires: time.Now().Add(10 * time.Hour),
-	}
-	http.SetCookie(w, cookie)
-	//w.Write([]byte(SID))
-
-}
-
-func (api *MyHandler) Logout(w http.ResponseWriter, r *http.Request) {
-
-	session, err := r.Cookie("session_id")
-	if err == http.ErrNoCookie {
-		http.Error(w, `no sess`, 401)
-		return
-	}
-
-	if _, ok := api.sessions[session.Value]; !ok {
-		http.Error(w, `no sess`, 401)
-		return
-	}
-
-	delete(api.sessions, session.Value)
-
-	session.Expires = time.Now().AddDate(0, 0, -1)
-	http.SetCookie(w, session)
-}
-
-func (api *MyHandler) Root(w http.ResponseWriter, r *http.Request) {
-	authorized := false
-	session, err := r.Cookie("session_id")
-	if err == nil && session != nil {
-		_, authorized = api.sessions[session.Value]
-	}
-
-	if authorized {
-		w.Write([]byte("autrorized"))
-	} else {
-		w.Write([]byte("not autrorized"))
-	}
-}
- */
-
 func main() {
 	r := mux.NewRouter()
 
@@ -159,17 +40,7 @@ func main() {
 	pinDelivery := httpDeliverPin.NewPinHandler(pinUsecase)
 
 	m := middleware.NewMiddleware(sessionUsecase)
-	//api := NewMyHandler()
-	/*sessionService := delivery.NewSessionHTTP()
-	sessionUseCase := usecase.NewSessionUseCase()
-	sessionService.SessionUseCase = sessionUseCase
 
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-
-	r.HandleFunc("/session/{id}", sessionService.GetSession).Methods("GET")*/
-	//r.PathPrefix("/sample-2020-lesson-2/public/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-
-	//r.HandleFunc("/", api.Root)
 	r.HandleFunc("/login", sessionDelivery.Login).Methods("POST")
 	r.HandleFunc("/logout", sessionDelivery.Logout)
 	r.HandleFunc("/signup", userDelivery.AddUser).Methods("POST")
