ab066ae4cde7a55472ec49033c66183788c32297
Name <Email>
2020-03-23 22:47:09 +0300

    base configs added

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,1 +1,55 @@
 package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+)
+
+//набор переменных для подключения
+var (
+	CONFIG = viper.New()
+	DB DataBaseConfig
+)
+
+
+type DataBaseConfig struct {
+	//TODO: заменить на строчку подключения, хранить имя драйвера
+	ConnString string `json:"db.connect"`
+	MaxConns int `json:"db.maxconns"`
+}
+
+func configinit()  {
+
+	CONFIG.SetConfigName("config")
+	CONFIG.AddConfigPath(".")
+	//TODO: добавить пути к конфигу
+
+	err := CONFIG.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %v \n", err))
+	}
+
+}
+
+func parseDbConfig(v *viper.Viper, config *DataBaseConfig) {
+	//обеспечивает
+	v.SetDefault("db.connect", "user=postgres password=password dbname=pinterest sslmode=disable")
+	v.SetDefault("db.maxconns", "20")
+
+	config.ConnString = v.GetString("db.connect")
+
+}
+
+func Start() {
+	CONFIG.SetConfigName("config")
+	CONFIG.AddConfigPath(".")
+
+	configinit()
+	parseDbConfig(CONFIG, &DB)
+
+	err := CONFIG.Unmarshal(&DB)
+	if err != nil {
+		log.Fatalf("decoding problem: , %v", err)
+	}
+}
