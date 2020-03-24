8b759d91401ec8a44a9d6a554ba5b0747bd4b9fe
Name <Email>
2020-03-25 02:29:49 +0300

    all funcs to work with pins

--- internal/pkg/database/db.go
+++ internal/pkg/database/db.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"2020_1_Color_noise/internal/models"
 	"2020_1_Color_noise/internal/pkg/config"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 //THIS IS FOR POSTGRES ONLY
@@ -50,4 +52,35 @@ func (db *PostgresSQL) Query(query string, args ...interface{}) (*sql.Rows, erro
 
 func (db *PostgresSQL) QueryRow(query string, args ...interface{}) *sql.Row {
 	return db.DB.QueryRow(query, args...)
+}
+
+//TODO:создание пина
+func (db *PostgresSQL) CreatePin(pin models.DataBasePin) *sql.Row {
+	return db.QueryRow("INSERT INTO pins(user_id, name, description, image, board_id, created_at) " +
+		"VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
+		pin.UserId, pin.Name, pin.Description, pin.Image, pin.BoardId, time.Now())
+}
+
+//TODO:обновление пина
+func (db *PostgresSQL) UpdatePin(pin models.DataBasePin) (sql.Result, error) {
+	return db.Exec("UPDATE pins SET" +
+	"name = $1, description = $2, board_id = $3" +
+	"WHERE id = &4", pin.Name, pin.Description, pin.BoardId, pin.Id)
+}
+
+func (db *PostgresSQL) DeletePin(pin models.DataBasePin) (sql.Result, error) {
+	return db.Exec("DELETE from pins WHERE id = $1", pin.Id)
+}
+
+func (db *PostgresSQL) GetPinById(id int) *sql.Row {
+	return db.QueryRow("SELECT * FROM pins WHERE id = $1", id)
+}
+
+func (db *PostgresSQL) GetPinsByUserId(id int) (*sql.Rows, error) {
+	return db.Query("SELECT * FROM pins WHERE user_id = $1", id)
+}
+
+//TODO: заменить на полнотекстовый поиск
+func (db *PostgresSQL) GetPinByName(name string) (*sql.Rows, error) {
+	return db.Query("SELECT * FROM pins WHERE name = $1", name)
 }
