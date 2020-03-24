a4f9d3aaa69c9696b4502d4847314472b284f854
Name <Email>
2020-03-25 02:28:07 +0300

    interface is now working with pin model

--- internal/pkg/database/interface.go
+++ internal/pkg/database/interface.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"2020_1_Color_noise/internal/models"
 	"2020_1_Color_noise/internal/pkg/config"
 	"database/sql"
 )
@@ -18,6 +19,13 @@ type DBInterface interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	//получить одно значение
 	QueryRow(query string, args ...interface{}) *sql.Row
+	//методы для пинов
+	CreatePin(pin models.DataBasePin) *sql.Row
+	UpdatePin(pin models.DataBasePin) (sql.Result, error)
+	DeletePin(pin models.DataBasePin) (sql.Result, error)
 
+	GetPinById(id int) *sql.Row
+	GetPinsByUserId(id int) (*sql.Rows, error)
+	GetPinByName(name string) (*sql.Rows, error)
 }
 
