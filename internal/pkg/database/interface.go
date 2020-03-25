6c72cce5d1a66a41a29714e160edc6a1bcff2103
Name <Email>
2020-03-25 22:32:02 +0300

    operations with db added

--- internal/pkg/database/interface.go
+++ internal/pkg/database/interface.go
@@ -24,8 +24,35 @@ type DBInterface interface {
 	UpdatePin(pin models.DataBasePin) (sql.Result, error)
 	DeletePin(pin models.DataBasePin) (sql.Result, error)
 
-	GetPinById(id int) *sql.Row
-	GetPinsByUserId(id int) (*sql.Rows, error)
-	GetPinByName(name string) (*sql.Rows, error)
+	GetPinById(pin models.DataBasePin) *sql.Row
+	GetPinsByUserId(pin models.DataBasePin) (*sql.Rows, error)
+	GetPinByName(pin models.DataBasePin) (*sql.Rows, error)
+
+	//методы для пользователей
+	CreateUser(user models.DataBaseUser)  *sql.Row
+	UpdateUser(user models.DataBaseUser)  (sql.Result, error)
+	UpdateUserDescription(user models.DataBaseUser)  (sql.Result, error)
+	UpdateUserPassword(user models.DataBaseUser)  (sql.Result, error)
+	UpdateUserAvatar(user models.DataBaseUser)  (sql.Result, error)
+	DeleteUser (user models.DataBaseUser) (sql.Result, error)
+	GetUserById(user models.DataBaseUser) *sql.Row
+	GetUserByLogin(user models.DataBaseUser) *sql.Row
+	GetUserByName(user models.DataBaseUser) *sql.Row
+	GetUserSubscriptions(user models.DataBaseUser) (*sql.Rows, error)
+	GetUserSubscribers(user models.DataBaseUser) (*sql.Rows, error)
+
+	//методы для досок
+	CreateBoard(board models.DataBaseBoard)  *sql.Row
+	UpdateBoard(board models.DataBaseBoard)  (sql.Result, error)
+	DeleteBoard(board models.DataBaseBoard) (sql.Result, error)
+	GetBoardById(board models.DataBaseBoard) *sql.Row
+	GetBoardsByUserId(board models.DataBaseBoard) (*sql.Rows, error)
+	GetBoardsByName(board models.DataBaseBoard) (*sql.Rows, error)
+
+	//методы для комментариев
+	CreateComment(cm models.DataBaseComment)  *sql.Row
+	UpdateComment(cm models.DataBaseComment) (sql.Result, error)
+	DeleteComment(cm models.DataBaseComment) (sql.Result, error)
+	GetCommentsByPinId(cm models.DataBaseComment) (*sql.Rows, error)
 }
 
