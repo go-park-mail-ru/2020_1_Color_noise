f8f7eb2dbf08b79ecd19dfcbeef708e0e4a2b723
ProEugene97 <proj9007@gmail.com>
2020-04-05 19:47:59 +0300

    added package list

---
+++ internal/pkg/list/delivery/usecase/usecase.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"2020_1_Color_noise/internal/models"
+	"2020_1_Color_noise/internal/pkg/list"
+	. "2020_1_Color_noise/internal/pkg/error"
+)
+
+type Usecase struct {
+	repo list.IRepository
+}
+
+func NewUsecase(repo list.IRepository) *Usecase {
+	return &Usecase{
+		repo: repo,
+	}
+}
+
+func (lu *Usecase) GetMainList(start int, limit int) ([]*models.Pin, error) {
+	pins, err := lu.repo.GetMainList(start, limit)
+	if err != nil {
+		return nil, Wrap(err, "GetMainList error")
+	}
+
+	return pins, nil
+}
+
+func (lu *Usecase) GetSubList(id uint, start int, limit int) ([]*models.Pin, error) {
+	pins, err := lu.repo.GetSubList(id, start, limit)
+	if err != nil {
+		return nil, Wrapf(err, "GetSubList by id error, pinId: %s", id)
+	}
+
+	return pins, nil
+}
+
+func (lu *Usecase) GetRecommendationList(id uint, start int, limit int) ([]*models.Pin, error) {
+	pins, err := lu.repo.GetRecommendationList(id, start, limit)
+	if err != nil {
+		return nil, Wrapf(err, "GetRecommendationList by id error, pinId: %s", id)
+	}
+
+	return pins, nil
+}
---
+++ internal/pkg/list/repository.go
@@ -0,0 +1,9 @@
+package list
+
+import "2020_1_Color_noise/internal/models"
+
+type IRepository interface {
+	GetMainList(start int, limit int) ([]*models.Pin, error)
+	GetSubList(id uint, start int, limit int) ([]*models.Pin, error)
+	GetRecommendationList(id uint, start int, limit int) ([]*models.Pin, error)
+}
