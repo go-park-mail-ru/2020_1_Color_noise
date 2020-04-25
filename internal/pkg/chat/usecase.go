2d5de6bd09dedb740057bb0f48d038bcc0f5e05b
ktlh <laciedreamer@gmail.com>
2020-04-25 19:31:58 +0300

    init chat version

---
+++ internal/pkg/chat/usecase.go
@@ -0,0 +1,5 @@
+package chat
+
+type IUsecase interface {
+
+}
---
+++ internal/pkg/chat/usecase/usecase.go
@@ -0,0 +1,10 @@
+package usecase
+
+import (
+	"2020_1_Color_noise/internal/pkg/chat"
+)
+
+type Usecase struct {
+	repo chat.IRepository
+}
+
