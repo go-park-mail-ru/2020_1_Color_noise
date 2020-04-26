2b5ccea773fbe360d26773edd733a0bd9bac05b0
ktlh <laciedreamer@gmail.com>
2020-04-27 02:09:12 +0300

    new notifications on top

--- internal/pkg/database/postgres_query.go
+++ internal/pkg/database/postgres_query.go
@@ -97,7 +97,7 @@ const (
 )
 
 const (
-	GetNoti = "SELECT message, from_user_id FROM notifies WHERE user_id = $1;"
+	GetNoti = "SELECT message, from_user_id FROM notifies WHERE user_id = $1 ORDER BY created_at DESC;"
 	PutNoti = "INSERT INTO notifies(" +
 		"user_id, message, from_user_id, created_at) VALUES($1, $2, $3, $4) RETURNING id;"
 )
