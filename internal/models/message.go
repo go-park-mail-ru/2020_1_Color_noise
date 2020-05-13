88e183c0c170fac3548e610f82a0015589c5d535
ktlh <laciedreamer@gmail.com>
2020-05-13 18:18:06 +0300

    easyjson update, models update

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,12 +1,23 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Message struct {
 	SendUser  *User
 	RecUser   *User
 	Message   string
 	Stickers  string
+	CreatedAt time.Time
+}
+
+type DMessage struct {
+	SendUser  *User
+	RecUser   *User
+	Message   sql.NullString
+	Stickers  sql.NullString
 	CreatedAt time.Time
 }
 
@@ -23,4 +34,42 @@ type ResponseMessage struct {
 	Message   string        `json:"message"`
 	Stickers  string        `json:"stickers"`
 	CreatedAt time.Time     `json:"created_at"`
+}
+
+func GetDMessage(message Message)  DMessage{
+	tmp := DMessage{
+		SendUser:  message.SendUser,
+		RecUser:   message.RecUser,
+		CreatedAt: message.CreatedAt,
+	}
+
+	if message.Message != "" {
+		tmp.Message.Valid = true
+		tmp.Message.String = message.Message
+	}
+
+	if message.Stickers != "" {
+		tmp.Stickers.Valid = true
+		tmp.Stickers.String = message.Stickers
+	}
+
+	return tmp
+}
+
+func GetMessage(message DMessage)  Message{
+	tmp := Message{
+		SendUser:  message.SendUser,
+		RecUser:   message.RecUser,
+		CreatedAt: message.CreatedAt,
+	}
+
+	if message.Message.Valid {
+		tmp.Message = message.Message.String
+	}
+
+	if message.Stickers.Valid {
+		tmp.Stickers = message.Stickers.String
+	}
+
+	return tmp
 }
