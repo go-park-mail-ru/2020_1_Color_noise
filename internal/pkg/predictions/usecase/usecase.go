02f922bf1bf70359b460f95d5744323bfd5f0ae0
ktlh <laciedreamer@gmail.com>
2020-05-29 14:51:55 +0300

    features

---
+++ internal/pkg/predictions/usecase/usecase.go
@@ -0,0 +1,51 @@
+package usecase
+
+import (
+	"encoding/json"
+	"github.com/mb-14/gomarkov"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+type Usecase struct {
+}
+
+var chain *gomarkov.Chain
+
+func init(){
+	chain = gomarkov.NewChain(1)
+	data, err := ioutil.ReadFile("model.json")
+	if err != nil {
+		log.Fatal("no data")
+	}
+
+	err = json.Unmarshal(data, &chain)
+	if err != nil {
+		log.Fatal("incorrect data")
+	}
+
+}
+
+func (us* Usecase) Predict(tags *[]string) (*[]string, error){
+
+	var generated []string
+
+	for _, i := range *tags {
+		for j := 0; j < 2; j++ {
+			tokens := []string{i}
+
+			for tokens[len(tokens)-1] != gomarkov.EndToken {
+				next, _ := chain.Generate(tokens[(len(tokens) - 1):])
+				tokens = append(tokens, next)
+			}
+			str := strings.Join(tokens[0:len(tokens)-1], " ")
+			if str != "" {
+				generated = append(generated, str)
+			}
+		}
+	}
+
+	return &generated, nil
+}
+
